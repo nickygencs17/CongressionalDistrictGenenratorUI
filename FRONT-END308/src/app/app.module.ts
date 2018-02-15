666304b9dc874b54b37fb799ee03bd603b639e93
nickygencs17 <nick_genco@hotmail.com>
2018-02-15 10:46:23 -0500

    added nav bar

--- FRONT-END308/src/app/app.module.ts
+++ FRONT-END308/src/app/app.module.ts
@@ -64,7 +64,10 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { AutoCompleteComponent } from './utilities/auto-complete/auto-complete.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatTooltipModule} from '@angular/material';
+import {
+  MatButtonModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatToolbar, MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
 import {LeafletModule} from "@asymmetrik/ngx-leaflet";
 
 @NgModule({
@@ -125,6 +128,7 @@ import {LeafletModule} from "@asymmetrik/ngx-leaflet";
     AutoCompleteComponent
   ],
   imports: [
+    MatToolbarModule,
     MatButtonModule,
     MatRadioModule,
     MatTooltipModule,
