78dd0d8698b9e281920c302c41860fae1ca44eb0
nickygencs17 <nick_genco@hotmail.com>
2018-03-03 15:35:31 -0500

    new.json

--- FRONT-END308/src/app/state/state.component.ts
+++ FRONT-END308/src/app/state/state.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from "../state.service";
 import {latLng, polygon, tileLayer} from "leaflet";
+import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-state',
@@ -9,10 +14,21 @@ import {latLng, polygon, tileLayer} from "leaflet";
 })
 export class StateComponent implements OnInit {
 
-  constructor(private state_service:StateService) { }
+  public geo_json_data;
+
+  constructor(private state_service:StateService, public http:HttpClient) { }
 
   ngOnInit() {
     console.log(this.state_service.state_id);
+    this.http.get('new.json')
+      .subscribe((data) => {
+          this.geo_json_data = data;
+          console.log(this.geo_json_data);
+        },
+        error => {
+          console.log(error.text());
+          alert('GEO JSON GET FAILED');
+        });
   }
 
 
