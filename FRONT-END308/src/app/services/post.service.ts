2d8f85ef6dde05416d60ef84b6d6456445c009a4
Rama Potturi <ramapotturi@Ramas-MBP.home>
2018-03-09 18:07:42 -0500

    blog

---
+++ FRONT-END308/src/app/services/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {  Post } from '../shared/post';
+import { POSTS } from '../shared/posts';
+
+@Injectable()
+export class PostService {
+
+  constructor() { }
+
+  getPosts(): Post[] {
+    return POSTS;
+  }
+
+  getPost(id: number): Post {
+    return POSTS.filter((post) => (post.id === id))[0];
+  }
+
+}
